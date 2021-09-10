a52116bcd850963d3c6f14a4fbc2493c3154a116
patmack14 <patmack16@gmail.com>
2021-09-10 11:07:29 -0500

    First commit need to figure out program flow

---
+++ numgen.py
@@ -0,0 +1,39 @@
+# numgen.py
+# Module to create arrays filled with random integers.
+#
+# Usage: python3 numgen.py filename quantity [rangesize]
+#
+# Created - 2021 January - Erik Steinmetz
+
+import numpy
+import sys
+
+def create_array( how_many, range_size) :
+    """ Creates an int array with the given number of spots. The array
+        is filled with int values within the range size, half negative.
+    """
+    halfsize = range_size // 2
+    answer = numpy.random.randint( -halfsize, halfsize, how_many)
+    return answer
+    
+def write_array( filename, data_array) :
+    """ Writes the given data array out to a file with the given filename.
+    """
+    outfile = open( filename, 'w')
+    for value in data_array :
+        aline = str(value) + "\n"
+        outfile.write( aline)
+    outfile.close()
+
+if __name__ == "__main__" :
+    if len(sys.argv) < 3 :
+        print( "Usage: python3 numgen.py filename quantity [rangesize]")
+        exit()
+    the_filename = sys.argv[1]
+    the_quantity = int(sys.argv[2])
+    the_rangesize = 200
+    if len(sys.argv) >= 4 :
+        the_rangesize = int( sys.argv[3])
+    the_data = create_array( the_quantity, the_rangesize)
+    write_array( the_filename, the_data)
+    print( the_quantity, "values written to", the_filename)
---
+++ quicksort.py
@@ -0,0 +1,30 @@
+import numgen
+import numpy
+
+def partition(array, lo, hi):
+    i = (lo - 1)
+    piv = array[hi]
+    
+    for jay in range(lo, hi):
+
+        if array[jay]<=piv:
+
+            i = i + 1
+            array[i], array[j]= array[j], array[i]
+
+    array[i+1], array[hi] = array[hi], array [i+1]
+
+    return (i+1)
+
+
+def quickSort(array, lo, hi):
+
+#if there's only 1 element, the array is sorted by default
+    if len(array) ==1:
+        return array
+    
+    if lo < hi:
+        py = partition(array, lo, hi)
+
+        quickSort(array, lo, py-1)
+        quickSort(array, py+1, hi)
